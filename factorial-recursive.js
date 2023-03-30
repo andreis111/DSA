84dcc4c96d5aa0b13550e00bf353bf871de4c64f
Andrei Serban <andrei.sserban@gmail.com>
2023-03-30 08:13:39 +0200

    Factorial Recursive ex

--- factorial-recursive.js
+++ factorial-recursive.js
@@ -1,12 +1,51 @@
-//To do: In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+//TO DO Factorial REDO
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
 
 // 5! = 5 * 4 * 3 * 2 * 1 = 120.
 
 // The value of 0! is 1.
 
-function factorial(n) {
+// #Your task
+
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+// //for loop
+// function factorialLoop(n) {
+//   let result = 1
+//   if (n < 2) {
+//     return result
+//   } else {
+//     for (i = 2; i <= n; i++) {
+//       result = i * result
+//     }
+
+//   }
+//   return result
+// }
+// console.log(factorialLoop(5));
+
+//   //recursive
+// function factorial(n){
+//     if(n < 2) {
+//       return 1
+//     }
+//     return n * factorial(n - 1)
+// }
+
+// console.log(factorial(5));
+
+//recursive+memoization
+const memo = {};
+function memoFactorial(n) {
   if (n < 2) {
     return 1;
   }
-  return n * factorial(n - 1);
+  if (memo[n]) {
+    return memo[n];
+  } else {
+    memo[n] = n * factorial(n - 1);
+    return memo[n];
+  }
 }
+console.log(memoFactorial(5));
